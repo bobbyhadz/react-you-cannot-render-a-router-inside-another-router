534109ca8b5e7bdfc43a10c0732fccaf462664b9
bobbyhadz <opmftw@gmail.com>
2024-04-07 10:42:07 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+import React from 'react';
+import {Route, Link, Routes} from 'react-router-dom';
+
+// 👇️ don't wrap in <Router>
+export default function App() {
+  return (
+    <div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
